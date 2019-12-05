9e1b8e5c3b705ca0570047a8743ea04c3a810d80
Edward Obert <56443800+EdwardObert@users.noreply.github.com>
2019-12-05 21:40:38 +0100

    Update typescript.ts

--- Aufgabe_7/typescript.ts
+++ Aufgabe_7/typescript.ts
@@ -1,148 +1,126 @@
-//kick
+ //Kick
 
-window.addEventListener("load", function() {
+    window.addEventListener("load", function() {
 
-    this.document.querySelector("#b1").addEventListener("mousedown", playSample)
+        document.querySelector("#b1").addEventListener("mousedown", function() {playSample("kick.mp3");
 
-    function playSample () {
-        var sound:HTMLAudioElement = new Audio("kick.mp3");
-        sound.play();
-        
-    }})
 
+
+    });
 
     //Snare
 
-window.addEventListener("load", function() {
-
-    this.document.querySelector("#b2").addEventListener("mousedown", playSample)
-
-    function playSample () {
-        var sound:HTMLAudioElement = new Audio("snare.mp3");
-        sound.play();
-    }})
-
-
-    //hihat
-
-    window.addEventListener("load", function() {
-
-        this.document.querySelector("#b3").addEventListener("mousedown", playSample)
     
-        function playSample () {
-            var sound:HTMLAudioElement = new Audio("hihat.mp3");
-            sound.play();
-        }})
 
+        document.querySelector("#b2").addEventListener("mousedown", function() { playSample("snare.mp3");
 
-//Bing1
+    });
 
-window.addEventListener("load", function() {
+        //haft
 
-    this.document.querySelector("#b4").addEventListener("mousedown", playSample)
-
-    function playSample () {
-        var sound:HTMLAudioElement = new Audio("A.mp3");
-        sound.play();
-    }})
-
-
-//B2
-
-window.addEventListener("load", function() {
-
-    this.document.querySelector("#b5").addEventListener("mousedown", playSample)
-
-    function playSample () {
-        var sound:HTMLAudioElement = new Audio("C.mp3");
-        sound.play();
-    }})
-
-
-    //B3
-
-    window.addEventListener("load", function() {
-
-        this.document.querySelector("#b6").addEventListener("mousedown", playSample)
     
-        function playSample () {
-            var sound:HTMLAudioElement = new Audio("F.mp3");
-            sound.play();
-        }})
+
+        document.querySelector("#play2").addEventListener("mousedown", function() { playSample("FFM2.mp3");
+
+    });
+
+
+    //High-Hat
+
+   
+
+        document.querySelector("#b3").addEventListener("mousedown", function() {playSample("hihat.mp3");
 
 
 
-        //B3
+   });
 
-        window.addEventListener("load", function() {
-
-            this.document.querySelector("#b7").addEventListener("mousedown", playSample)
-        
-            function playSample () {
-                var sound:HTMLAudioElement = new Audio("G.mp3");
-                sound.play();
-            }})
+     //Tone F
 
 
-    //lache
 
-    window.addEventListener("load", function() {
-
-        this.document.querySelector("#b8").addEventListener("mousedown", playSample)
-    
-        function playSample () {
-            var sound:HTMLAudioElement = new Audio("laugh-1.mp3");
-            sound.play();
-        }})
-
-    //lachee
-
-    window.addEventListener("load", function() {
-
-        this.document.querySelector("#b9").addEventListener("mousedown", playSample)
-    
-        function playSample () {
-            var sound:HTMLAudioElement = new Audio("laugh-2.mp3");
-            sound.play();
-        }})
+        document.querySelector("#b4").addEventListener("mousedown", function() {playSample("F.mp3");
 
 
-    //snare
-
-    window.addEventListener("load", function() {
-
-        this.document.querySelector("#b9").addEventListener("mousedown", playSample)
-    
-        function playSample () {
-            var sound:HTMLAudioElement = new Audio("snare.mp3");
-            sound.play();
-        }})
 
 
-    //spaghetti and meatballsssss
 
-    window.addEventListener("load", function() {
 
-        this.document.querySelector("#b10").addEventListener("mousedown", playSample)
-    
-        function playSample () {
-            var sound:HTMLAudioElement = new Audio("spaghetti.mp3");
-            sound.play();
-        }})
+   });
+
+
+ //Tone G
+
+
+    document.querySelector("#b5").addEventListener("mousedown", function() {playSample("G.mp3");
+
+});
+
+ //Tone A
+
+
+
+    document.querySelector("#b6").addEventListener("mousedown", function() {playSample("A.mp3");
+
+
+});
+
+//Tone C
+
+
+
+    document.querySelector("#b7").addEventListener("mousedown", function() {playSample("C.mp3");
+
+
+
+});
+
+//Lache
+
+
+
+    document.querySelector("#b8").addEventListener("mousedown", function() {playSample("laugh-1.mp3");
+
+
+
+});
+
+//Lachee
+
+
+    document.querySelector("#b9").addEventListener("mousedown", function() { playSample("laugh-2.mp3");
+
+
+
+
+
+});
+
+//special
+
+
+document.querySelector("#b10").addEventListener("mousedown", function() { playSample("spaghetti.mp3");
+
+
+
+
+
+});
 
 
 //abfahrt
 
-window.addEventListener("load", function(Beat) {
+
 
     document.querySelector("#play").addEventListener("click", StartBeat)
 
+} );
 
+function playSample (PlayBeat: string){
 
+    var sound:HTMLAudioElement = new Audio("assets/" + PlayBeat);
+    sound.play();};
 
-
-
-} )
 
 function StartBeat() {
     var inter = setInterval(Beat,300)
@@ -155,20 +133,17 @@ function Beat () {
     if (index>11) index=0;
     console.log(myBeatyo [index]);
 
-}
+};
 
-}
 
+    }
 //kleiner Gag
 
 window.addEventListener("load", function() {
 
-    this.document.querySelector("#play2").addEventListener("mousedown", playSample)
+    this.document.querySelector("#play2").addEventListener("mousedown", playHaft)
 
-    function playSample () {
+    function playHaft () {
         var sound:HTMLAudioElement = new Audio("FFM2.mp3");
         sound.play();
     }})
-
-
-
